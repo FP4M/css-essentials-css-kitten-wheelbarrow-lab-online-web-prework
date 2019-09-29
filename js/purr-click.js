9b7ba017d4bec6e4a25e6ad465f38ffc7d7fe8fe
Prince Charles <princecharles35@yahoo.com>
2019-09-29 01:36:58 +0000

    Automatically backed up by Learn

--- js/purr-click.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  var player1 = document.getElementById('player1');
-  $('img').click(function(){
-    player1.play();
-  });
-});
-159.203.117.55:3107. Navigate to 159.203.117.55:3107
-
-
